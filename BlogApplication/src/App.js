1913fc6812adf69486163f517c44c95c7a645b50
Jorge Villacreces <jorgevillacreces1@gmail.com>
2023-03-25 10:07:33 -0400

    Working with custom hooks

--- BlogApplication/src/App.js
+++ BlogApplication/src/App.js
@@ -11,6 +11,7 @@ import {Route, Routes, useNavigate} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import {format } from 'date-fns';
 import api from './api/posts'
+import {useWindowSize} from './hooks/useWindowSize';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
   const [editTitle, setEditTitle] = useState('');
   const [editBody, setEditBody] = useState('');
   const navigate = useNavigate();
+  const {width} = useWindowSize();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -94,7 +96,7 @@ function App() {
   }
   return (
     <div className="App">
-      <Header title="React JS Blog"/>
+      <Header title="React JS Blog" width={width}/>
       <Nav search={search} setSearch={setSearch}/>
       <Routes>
         <Route path='/' element={<Home posts={searchResults}/>}/>
